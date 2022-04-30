915ebaa155800545033676dbc9e679a3b6d49220
Jonas Nelle <jonas.a.nelle@gmail.com>
2022-04-30 17:26:46 -0400

    add fast tests for oracle implementation

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,22 +4,28 @@ from starkware.crypto.signature.signature import (
     get_random_private_key,
     private_to_stark_key,
 )
+import asyncio
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
+def event_loop():
+    return asyncio.new_event_loop()
+
+
+@pytest.fixture(scope="module")
 def private_and_public_publisher_keys():
     publisher_private_key = get_random_private_key()
     publisher_public_key = private_to_stark_key(publisher_private_key)
     return publisher_private_key, publisher_public_key
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def publisher():
     return str_to_felt("foo")
 
 
-@pytest.fixture
-def private_and_public_registration_keys():
-    registration_private_key = get_random_private_key()
-    registration_public_key = private_to_stark_key(registration_private_key)
-    return registration_private_key, registration_public_key
+@pytest.fixture(scope="module")
+def private_and_public_admin_keys():
+    admin_private_key = get_random_private_key()
+    admin_public_key = private_to_stark_key(admin_private_key)
+    return admin_private_key, admin_public_key
