8c5d9038f279da1a8de55ae5ce9599111550b246
Jonas Nelle <jonas.a.nelle@gmail.com>
2022-05-18 18:17:13 -0400

    redeploy contracts and update readme

--- sample-publisher/utils/add_oracle_implementation.py
+++ sample-publisher/utils/add_oracle_implementation.py
@@ -8,7 +8,7 @@ async def main():
     admin_private_key = int(os.environ.get("ADMIN_PRIVATE_KEY"))
     admin_client = PontisAdminClient(admin_private_key)
     result = await admin_client.add_oracle_implementation(
-        1756514290914178884475067584566382164070297082487069321591772071402623854458
+        505099763246281825999240702363847539949296834022127438235264284507627799291
     )
 
     print(f"Added oracle implementation with transaction {result}")
