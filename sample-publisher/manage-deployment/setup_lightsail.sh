365c784a07fa648759b879c0aa8d4b041b2050b8
Jonas Nelle <jonas.a.nelle@gmail.com>
2022-05-22 20:08:21 -0400

    new lightsail instance

--- sample-publisher/manage-deployment/setup_lightsail.sh
+++ sample-publisher/manage-deployment/setup_lightsail.sh
@@ -1,4 +1,4 @@
-ssh -i LightsailDefaultKey-us-east-2.pem ubuntu@18.190.153.10 -t "mkdir all"
-scp -i LightsailDefaultKey-us-east-2.pem -r ../all/ ubuntu@18.190.153.10:
-scp -i LightsailDefaultKey-us-east-2.pem -r initialize_lightsail.sh ubuntu@18.190.153.10:
-ssh -i LightsailDefaultKey-us-east-2.pem ubuntu@18.190.153.10 -t "source initialize_lightsail.sh"
+ssh -i LightsailDefaultKey-us-east-2.pem ubuntu@52.14.202.151 -t "mkdir all"
+scp -i LightsailDefaultKey-us-east-2.pem -r ../all/ ubuntu@52.14.202.151:
+scp -i LightsailDefaultKey-us-east-2.pem -r initialize_lightsail.sh ubuntu@52.14.202.151:
+ssh -i LightsailDefaultKey-us-east-2.pem ubuntu@52.14.202.151 -t "source initialize_lightsail.sh"
