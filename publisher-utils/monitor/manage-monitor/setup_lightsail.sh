03f56848635b4fe48671d5a02c2170c1ad1b4ecc
Jonas Nelle <jonas.a.nelle@gmail.com>
2022-06-05 12:49:48 -0400

    update lightsail ip

--- publisher-utils/monitor/manage-monitor/setup_lightsail.sh
+++ publisher-utils/monitor/manage-monitor/setup_lightsail.sh
@@ -1,4 +1,4 @@
-ssh -i LightsailDefaultKey-us-east-2.pem ubuntu@52.14.202.151 -t "mkdir monitor"
-scp -i LightsailDefaultKey-us-east-2.pem -r ../monitor/ ubuntu@52.14.202.151:
-scp -i LightsailDefaultKey-us-east-2.pem -r initialize_lightsail.sh ubuntu@52.14.202.151:
-ssh -i LightsailDefaultKey-us-east-2.pem ubuntu@52.14.202.151 -t "source initialize_lightsail.sh"
+ssh -i LightsailDefaultKey-us-east-2.pem ubuntu@18.224.31.134 -t "mkdir monitor"
+scp -i LightsailDefaultKey-us-east-2.pem -r ../../monitor/ ubuntu@18.224.31.134:
+scp -i LightsailDefaultKey-us-east-2.pem -r initialize_lightsail.sh ubuntu@18.224.31.134:
+ssh -i LightsailDefaultKey-us-east-2.pem ubuntu@18.224.31.134 -t "source initialize_lightsail.sh"
