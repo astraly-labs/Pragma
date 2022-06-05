58b63ea013e86524bbdc21bf512a0ce6e5e47c45
Jonas Nelle <jonas.a.nelle@gmail.com>
2022-06-05 12:49:40 -0400

    fix crontab writing

--- publisher-utils/monitor/manage-monitor/initialize_lightsail.sh
+++ publisher-utils/monitor/manage-monitor/initialize_lightsail.sh
@@ -11,4 +11,4 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 apt-cache policy docker-ce
 sudo apt install -y docker-ce
 sudo usermod -aG docker ${USER}
-(crontab -l 2>/dev/null; echo "0 * * * * docker build --pull monitor/balance -t monitor-balance && docker run --rm --env-file monitor/balance/.env monitor-balance > monitor/log-balance.txt 2>&1 \n 0 * * * * docker build --pull monitor/price -t monitor-price && docker run --rm --env-file monitor/price/.env monitor-price > monitor/log-price.txt 2>&1") | crontab -
+(crontab -l 2>/dev/null; echo -e "0 * * * * docker build --pull monitor/balance -t monitor-balance && docker run --rm --env-file monitor/balance/.env monitor-balance > monitor/log-balance.txt 2>&1 \n0 * * * * docker build --pull monitor/price -t monitor-price && docker run --rm --env-file monitor/price/.env monitor-price > monitor/log-price.txt 2>&1") | crontab -
