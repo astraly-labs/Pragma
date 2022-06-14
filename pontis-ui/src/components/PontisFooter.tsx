b292140bc77a9eed37e11d17c1c73dbd056a1949
Raphael <raphael.t.ruban@gmail.com>
2022-06-14 15:58:35 -0400

    Changed footer

--- pontis-ui/src/components/PontisFooter.tsx
+++ pontis-ui/src/components/PontisFooter.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 import { getOracleControllerAddress } from "../services/address.service";
 import {
   buildExplorerUrlForAddress,
@@ -8,16 +9,28 @@ import { StyledExternalLink } from "./StyledLink";
 
 const PontisFooter = () => {
   return (
-    <div className="w-full py-6 mt-auto text-center text-lg">
-      <StyledExternalLink
-        href={buildExplorerUrlForAddress(
-          getOracleControllerAddress(networkId())
-        )}
-        target="_blank"
-      >
-        View on Voyager
-      </StyledExternalLink>
-    </div>
+    <section className="text-gray-900">
+      <div className="container flex flex-col items-center p-8 mx-auto max-w-7xl border-t border-gray-400 sm:flex-row">
+        <Link href="/">
+          <a>
+            <img src="/pontis-logo.svg" className="h-8" alt="Pontis Logo" />
+          </a>
+        </Link>
+        <p className="mt-4 text-sm sm:pl-4 sm:mt-0 sm:ml-4 sm:border-l sm:border-gray-400">
+          © {new Date().getFullYear()} - 42 Labs
+        </p>
+        <span className="inline-flex justify-center mt-4 space-x-5 sm:justify-start sm:mt-0 sm:ml-auto">
+          <StyledExternalLink
+            href={buildExplorerUrlForAddress(
+              getOracleControllerAddress(networkId())
+            )}
+            target="_blank"
+          >
+            View on Voyager
+          </StyledExternalLink>
+        </span>
+      </div>
+    </section>
   );
 };
 
