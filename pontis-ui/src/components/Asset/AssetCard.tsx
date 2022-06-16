48dc82043fe33c74b3834e57e5e2d511e317c897
Raphael <raphael.t.ruban@gmail.com>
2022-06-15 20:15:51 -0400

    Turned asset card into grid

--- pontis-ui/src/components/Asset/AssetCard.tsx
+++ pontis-ui/src/components/Asset/AssetCard.tsx
@@ -15,8 +15,10 @@ const AssetCard: React.FC<AssetCardProps> = ({ assetKey }) => {
   const { oracleResponse, error } = useOracleGetValue(assetKey);
   return (
     <div className="bg-slate-50 shadow-lg hover:shadow-xl rounded-xl flex flex-row overflow-hidden w-full cursor-pointer">
-      <div className="w-full max-w-full flex flex-row flex-wrap justify-between items-center text-slate-900 mx-4 py-4">
-        <AssetName assetKey={assetKey} />
+      <div className="w-full max-w-full grid grid-cols-2 sm:grid-cols-3 grid-rows-2 sm:grid-rows-1 text-slate-900 mx-4 py-4 gap-y-3">
+        <div className="place-self-start col-span-1 row-span-1">
+          <AssetName assetKey={assetKey} />
+        </div>
         {/* {error === undefined ? (
           <React.Fragment>
             <AssetCardPrice response={oracleResponse} />
@@ -36,10 +38,14 @@ const AssetCard: React.FC<AssetCardProps> = ({ assetKey }) => {
             <div>No results found for {assetKey.toLocaleUpperCase()}</div>
           ) : (
             <React.Fragment>
-              <AssetCardPrice price={oracleResponse.value} />
-              <AssetCardTime
-                lastUpdatedTimestamp={oracleResponse.lastUpdatedTimestamp}
-              />
+              <div className="col-start-2 col-span-1 row-start-1 row-span-2 flex justify-end items-start sm:row-span-1 sm:place-self-center">
+                <AssetCardPrice price={oracleResponse.value} />
+              </div>
+              <div className="col-start-1 row-start-2 row-span-1 col-span-1 sm:col-start-3 sm:row-start-1 place-self-start sm:place-self-end">
+                <AssetCardTime
+                  lastUpdatedTimestamp={oracleResponse.lastUpdatedTimestamp}
+                />
+              </div>
             </React.Fragment>
           )
         ) : error !== undefined ? (
