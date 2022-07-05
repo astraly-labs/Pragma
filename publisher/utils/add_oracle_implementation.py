1e5dc3d1be1793531b330814a7aa9e5d6613f7eb
Jonas Nelle <jonas.a.nelle@gmail.com>
2022-07-05 17:49:26 +0200

    update staging addresses

--- publisher/utils/add_oracle_implementation.py
+++ publisher/utils/add_oracle_implementation.py
@@ -7,9 +7,14 @@ from pontis.admin.client import PontisAdminClient
 async def main():
     admin_private_key = int(os.environ.get("ADMIN_PRIVATE_KEY"))
     admin_client = PontisAdminClient(admin_private_key)
-    await admin_client.add_oracle_implementation(
-        0x02BD808487154776F5275DCDB506C9D55319778567D4460F0C0BBBF42F482370
-    )
+    if os.environ.get("__PONTIS_STAGING_ENV__") == "TRUE":
+        await admin_client.add_oracle_implementation(
+            0x052392D7B748DFBD81314FA41D6BBD6DD528D6C146BB6B212CCE72FB2B5D5801
+        )
+    else:
+        await admin_client.add_oracle_implementation(
+            0x02BD808487154776F5275DCDB506C9D55319778567D4460F0C0BBBF42F482370
+        )
 
 
 if __name__ == "__main__":
--- publisher/utils/set_primary_oracle_implementation.py
+++ publisher/utils/set_primary_oracle_implementation.py
@@ -9,9 +9,14 @@ async def main():
     admin_client = PontisAdminClient(
         admin_private_key,
     )
-    await admin_client.set_primary_oracle_implementation_address(
-        0x02BD808487154776F5275DCDB506C9D55319778567D4460F0C0BBBF42F482370
-    )
+    if os.environ.get("__PONTIS_STAGING_ENV__") == "TRUE":
+        await admin_client.set_primary_oracle_implementation_address(
+            0x052392D7B748DFBD81314FA41D6BBD6DD528D6C146BB6B212CCE72FB2B5D5801
+        )
+    else:
+        await admin_client.set_primary_oracle_implementation_address(
+            0x02BD808487154776F5275DCDB506C9D55319778567D4460F0C0BBBF42F482370
+        )
 
 
 if __name__ == "__main__":
