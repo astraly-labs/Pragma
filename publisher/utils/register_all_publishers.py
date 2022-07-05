64b51f5012378ce8571f6708946bc2e3a9d6d28e
Jonas Nelle <jonas.a.nelle@gmail.com>
2022-07-05 10:36:14 +0200

    update publisher util scripts to handle source/publisher separation

--- publisher/utils/register_all_publishers.py
+++ publisher/utils/register_all_publishers.py
@@ -3,17 +3,7 @@ import os
 
 from pontis.admin.client import PontisAdminClient
 
-publishers = [
-    "pontis-binance",
-    "pontis-bitstamp",
-    "pontis-cex",
-    "pontis-coinbase",
-    "pontis-coingecko",
-    "pontis-coinmarketcap",
-    "pontis-ftx",
-    "pontis-gemini",
-    "pontis-thegraph",
-]
+publisher = "pontis"
 publisher_address = int(os.environ.get("PUBLISHER_ADDRESS"))
 
 
@@ -22,10 +12,9 @@ async def main():
     admin_client = PontisAdminClient(
         admin_private_key,
     )
-    for publisher in publishers:
-        await admin_client.register_publisher_if_not_registered(
-            publisher, publisher_address
-        )
+    await admin_client.register_publisher_if_not_registered(
+        publisher, publisher_address
+    )
 
 
 if __name__ == "__main__":
--- publisher/utils/register_publisher.py
+++ publisher/utils/register_publisher.py
@@ -3,20 +3,8 @@ import os
 
 from pontis.admin.client import PontisAdminClient
 
-publishers = [
-    f"equilibrium-{source}"
-    for source in [
-        "coinmarketcap",
-        "coingecko",
-        "coinbase",
-        "gemini",
-        "binance",
-        "ftx",
-        "cex",
-        "bitstamp",
-    ]
-]
-publishers = ["cmt"]
+publisher = "equilibrium"
+publisher = "cmt"
 publisher_address = 0x02FE2CA4C677AF0FF601A5AA37F799701C3ECD6F5D04F2EE072CB4C37FDFCDEF
 
 
@@ -25,10 +13,9 @@ async def main():
     admin_client = PontisAdminClient(
         admin_private_key,
     )
-    for publisher in publishers:
-        await admin_client.register_publisher_if_not_registered(
-            publisher, publisher_address
-        )
+    await admin_client.register_publisher_if_not_registered(
+        publisher, publisher_address
+    )
 
 
 if __name__ == "__main__":
