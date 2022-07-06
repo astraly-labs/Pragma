77d6151c9d2849849cb7a43f49d1d55d6cedd41f
Jonas Nelle <jonas.a.nelle@gmail.com>
2022-07-06 17:03:06 +0200

    fix deployment region

--- publisher/manage-deployment/setup_lightsail.sh
+++ publisher/manage-deployment/setup_lightsail.sh
@@ -1,4 +1,4 @@
-ssh -i LightsailDefaultKey-us-east-2.pem ubuntu@3.237.67.239 -t "mkdir all"
-scp -i LightsailDefaultKey-us-east-2.pem -r ../sample-publisher/all/ ubuntu@3.237.67.239:
-scp -i LightsailDefaultKey-us-east-2.pem -r initialize_lightsail.sh ubuntu@3.237.67.239:
-ssh -i LightsailDefaultKey-us-east-2.pem ubuntu@3.237.67.239 -t "source initialize_lightsail.sh"
+ssh -i LightsailDefaultKey-us-east-1.pem ubuntu@3.237.67.239 -t "mkdir all"
+scp -i LightsailDefaultKey-us-east-1.pem -r ../sample-publisher/all/ ubuntu@3.237.67.239:
+scp -i LightsailDefaultKey-us-east-1.pem -r initialize_lightsail.sh ubuntu@3.237.67.239:
+ssh -i LightsailDefaultKey-us-east-1.pem ubuntu@3.237.67.239 -t "source initialize_lightsail.sh"
