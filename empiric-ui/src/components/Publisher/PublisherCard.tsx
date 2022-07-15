47b8510ed52e8c0f2915b79fef064c48277f55d1
Raphael <raphael.t.ruban@gmail.com>
2022-07-15 16:33:23 +0200

    Removed console.log

--- empiric-ui/src/components/Publisher/PublisherCard.tsx
+++ empiric-ui/src/components/Publisher/PublisherCard.tsx
@@ -7,32 +7,29 @@ const PublisherCard: React.FC<PublisherCardProps> = ({
   name,
   description,
   href,
-}) => {
-  console.log(href ? "a" : "b");
-  return (
-    <figure className="relative h-full rounded-lg bg-white p-6 shadow-xl shadow-slate-900/10">
-      <figcaption className="relative flex flex-col items-center justify-between border-b border-slate-100 pb-6">
-        {href ? (
-          <a href={href}>
-            <img src={src} alt={`${name} logo`} className="h-12 w-auto" />
-          </a>
-        ) : (
+}) => (
+  <figure className="relative h-full rounded-lg bg-white p-6 shadow-xl shadow-slate-900/10">
+    <figcaption className="relative flex flex-col items-center justify-between border-b border-slate-100 pb-6">
+      {href ? (
+        <a href={href}>
           <img src={src} alt={`${name} logo`} className="h-12 w-auto" />
-        )}
-      </figcaption>
-      <div className="relative mt-6">
-        <h3 className="relative mb-3 text-2xl font-semibold text-slate-900">
-          {name}
-        </h3>
-        <blockquote className="relative">
-          <QuoteIcon className="absolute top-0 left-0 fill-slate-100" />
-          <p className="prose prose-slate relative whitespace-pre-line">
-            {description}
-          </p>
-        </blockquote>
-      </div>
-    </figure>
-  );
-};
+        </a>
+      ) : (
+        <img src={src} alt={`${name} logo`} className="h-12 w-auto" />
+      )}
+    </figcaption>
+    <div className="relative mt-6">
+      <h3 className="relative mb-3 text-2xl font-semibold text-slate-900">
+        {name}
+      </h3>
+      <blockquote className="relative">
+        <QuoteIcon className="absolute top-0 left-0 fill-slate-100" />
+        <p className="prose prose-slate relative whitespace-pre-line">
+          {description}
+        </p>
+      </blockquote>
+    </div>
+  </figure>
+);
 
 export default PublisherCard;
