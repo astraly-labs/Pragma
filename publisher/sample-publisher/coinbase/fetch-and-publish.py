271c1013c7896e7565328c0d92829b2401930f2d
Jonas Nelle <jonas.a.nelle@gmail.com>
2022-07-15 11:02:50 +0200

    fix

--- publisher/sample-publisher/coinbase/fetch-and-publish.py
+++ publisher/sample-publisher/coinbase/fetch-and-publish.py
@@ -7,7 +7,7 @@ from empiric.publisher.fetch import fetch_coinbase
 
 
 async def main():
-    publisher = int(os.environ.get("PUBLISHER"), 0)
+    publisher = os.environ.get("PUBLISHER")
     publisher_private_key = int(os.environ.get("PUBLISHER_PRIVATE_KEY"), 0)
     publisher_address = int(os.environ.get("PUBLISHER_ADDRESS"), 0)
 
