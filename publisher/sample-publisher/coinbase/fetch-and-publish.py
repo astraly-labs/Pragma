fde8e513a5340c5570c924fdd8e5f187ba69b4b4
Jonas Nelle <jonas.a.nelle@gmail.com>
2022-07-15 10:59:48 +0200

    fix coinbase publisher and add to tests

--- publisher/sample-publisher/coinbase/fetch-and-publish.py
+++ publisher/sample-publisher/coinbase/fetch-and-publish.py
@@ -5,16 +5,13 @@ from empiric.publisher.assets import EMPIRIC_ALL_ASSETS
 from empiric.publisher.client import EmpiricPublisherClient
 from empiric.publisher.fetch import fetch_coinbase
 
-DECIMALS = 18
-
 
 async def main():
+    publisher = int(os.environ.get("PUBLISHER"), 0)
     publisher_private_key = int(os.environ.get("PUBLISHER_PRIVATE_KEY"), 0)
     publisher_address = int(os.environ.get("PUBLISHER_ADDRESS"), 0)
 
-    assets = EMPIRIC_ALL_ASSETS
-
-    entries = fetch_coinbase(assets)
+    entries = fetch_coinbase(EMPIRIC_ALL_ASSETS, publisher)
 
     publisher_client = EmpiricPublisherClient(publisher_private_key, publisher_address)
     await publisher_client.publish_many(entries)
