0483af99b641e6baa6a6157de4cf54a428af285a
Jonas Nelle <jonas.a.nelle@gmail.com>
2022-07-25 18:02:25 -0400

    run monitor eveery 10 minutes

--- publisher/monitor/manage-monitor/initialize_lightsail.sh
+++ publisher/monitor/manage-monitor/initialize_lightsail.sh
@@ -11,4 +11,4 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 apt-cache policy docker-ce
 sudo apt install -y docker-ce
 sudo usermod -aG docker ${USER}
-(crontab -l 2>/dev/null; echo -e "0 * * * * docker build monitor/balance -t monitor-balance && docker run --rm --env-file monitor/balance/.env --env-file monitor/balance/.secrets.env monitor-balance > monitor/log-balance.txt 2>&1 \n0 * * * * docker build monitor/price -t monitor-price && docker run --rm --env-file monitor/price/.env --env-file monitor/price/.secrets.env monitor-price > monitor/log-price.txt 2>&1") | crontab -
+(crontab -l 2>/dev/null; echo -e "*/10 * * * * docker build monitor/balance -t monitor-balance && docker run --rm --env-file monitor/balance/.env --env-file monitor/balance/.secrets.env monitor-balance > monitor/log-balance.txt 2>&1 \n*/10 * * * * docker build monitor/price -t monitor-price && docker run --rm --env-file monitor/price/.env --env-file monitor/price/.secrets.env monitor-price > monitor/log-price.txt 2>&1") | crontab -
