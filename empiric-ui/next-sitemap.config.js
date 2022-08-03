a4182e6bbcb825f83f03bb9363b1f2196c0e2a45
Raphael <raphael.t.ruban@gmail.com>
2022-08-03 13:53:28 -0400

    Rework sitemap

--- empiric-ui/next-sitemap.config.js
+++ empiric-ui/next-sitemap.config.js
@@ -1,8 +1,24 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
   siteUrl: process.env.SITE_URL || "https://empiric.network",
-  generateRobotsTxt: true, // (optional)
   // ...other options
+  generateRobotsTxt: true,
+  changefreq: "weekly",
+  priority: 0.6,
+  exclude: ["/details/*"],
+  generateIndexSitemap: false, // remove this line once we link to over 5000 urls
+
+  transform: async (config, path) => {
+    // Use default transformation
+    // returning partial properties will result in generation of XML field with only returned values.
+    return {
+      loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+      changefreq: config.changefreq,
+      priority: path === "/" ? 0.8 : config.priority, // Give homepage higher priority
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    };
+  },
 };
 
 module.exports = config;
