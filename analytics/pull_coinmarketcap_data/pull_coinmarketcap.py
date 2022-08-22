8641e32950e5f3e04370f7731c622a1656c5c587
Jonas Nelle <jonas.a.nelle@gmail.com>
2022-08-22 17:06:14 -0400

    add coinmarketcap data pulling code for benchmark

---
+++ analytics/pull_coinmarketcap_data/pull_coinmarketcap.py
@@ -0,0 +1,45 @@
+import os
+import time
+import requests
+import datetime
+
+import pandas as pd
+from starknet_py.contract import Contract
+from starknet_py.net.gateway_client import GatewayClient
+
+
+def main():
+    print(f"Fetching Coinmarketcap data at time: {time.time()}")
+    data_output_filename = "coinmarketcap_data.csv"
+
+    COINMARKETCAP_KEY = os.environ.get("COINMARKETCAP_API_KEY")
+    response = requests.get(
+        "https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest",
+        headers={
+            "X-CMC_PRO_API_KEY": COINMARKETCAP_KEY,
+            "Accepts": "application/json",
+        },
+        params={"slug": "ethereum"},
+    )
+    result = {
+        "value": response.json()["data"]["1027"]["quote"]["USD"]["price"],
+        "timestamp": int(
+            datetime.datetime.strptime(
+                response.json()["data"]["1027"]["quote"]["USD"]["last_updated"],
+                "%Y-%m-%dT%H:%M:%S.%f%z",
+            ).timestamp()
+        ),
+    }
+
+    df = pd.DataFrame(result, index=[0])
+    if not os.path.isfile(data_output_filename):
+        df.to_csv(data_output_filename)
+    else:
+        df.to_csv(data_output_filename, mode="a", header=False)
+
+
+if __name__ == "__main__":
+    while True:
+        start_time = time.time()
+        main()
+        time.sleep(time.time() - start_time + 30)  # sleep for 30 seconds
