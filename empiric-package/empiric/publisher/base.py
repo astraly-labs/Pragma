8da6b6f317b4b3c922286a962d63b7da663c5246
rlkelly <robert.l.kelly3@gmail.com>
2022-09-14 22:27:45 -0400

    circular import erorr

--- empiric-package/empiric/publisher/base.py
+++ empiric-package/empiric/publisher/base.py
@@ -2,12 +2,11 @@ import abc
 from typing import List
 
 from aiohttp import ClientSession
-from empiric.core.entry import Entry
 
 
 class PublisherInterfaceT(abc.ABC):
     @abc.abstractmethod
-    async def fetch(self, session: ClientSession) -> List[Entry]:
+    async def fetch(self, session: ClientSession) -> List["Entry"]:
         ...
 
 
