143472ab7c07f60ef2aefc51622025dc7d3b7097
Jonas Nelle <jonas@42labs.xyz>
2022-10-05 13:42:59 -0400

    fix cex fetcher name

--- stagecoach/integration_tests/cex/cex_publisher.py
+++ stagecoach/integration_tests/cex/cex_publisher.py
@@ -19,8 +19,8 @@ async def main():
         account_private_key=publisher_private_key,
         account_contract_address=publisher_address,
     )
-    coinbase_fetcher = CexFetcher(EMPIRIC_ALL_ASSETS, publisher)
-    publisher_client.add_fetcher(coinbase_fetcher)
+    cex_fetcher = CexFetcher(EMPIRIC_ALL_ASSETS, publisher)
+    publisher_client.add_fetcher(cex_fetcher)
     _entries = await publisher_client.fetch()
 
     response = await publisher_client.publish_many(_entries, pagination=20)
