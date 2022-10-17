964ba4b6315f551045e5fd74731615b863e28371
abdalisara77 <abdalisara77@gmail.com>
2022-10-17 11:48:03 -0400

    fixed style issues, added hint types

--- analytics/compfeeds_analytics/lending_borrowing/dynamicLiquidator.py
+++ analytics/compfeeds_analytics/lending_borrowing/dynamicLiquidator.py
@@ -1,36 +1,42 @@
-import pandas as pd
-from datetime import datetime
-import time
-import matplotlib.pyplot as plt
-import numpy as np
 import utils
-from matplotlib import style
-import matplotlib.dates as md 
-import scipy 
-
-class dynamicLiquidator:
-    def __init__(self, dynamicLBProtocol): 
-        self.protocol = dynamicLBProtocol 
-        self.reward = 0 
+from dynamicLB import DynamicLBProtocol
 
 
-    def check_liquidity_opportunities(self, dynamicLBProtocol): 
-        if dynamicLBProtocol.recoveryMode:
-            while recoveryMode:
-                liquidate(dynamicLBProtocol, 0)
-        else: 
-            for i in range(len(dynamicLBProtocol.balanceSheet)): 
-                if (dynamicLBProtocol.balanceSheet[i,2] / dynamicLBProtocol.balanceSheet[i,1]) < dynamicLBProtocol.calculate_liquidation_threshold(self, dynamicLBProtocol.balanceSheet[i,2]): 
-                    liquidate(dynamicLBProtocol, i)       
+class DynamicLiquidator:
+    def __init__(self, protocol: DynamicLBProtocol):
+        self.protocol = protocol
+        self.reward = 0
 
+    def check_liquidity_opportunities(self, protocol: DynamicLBProtocol):
+        if protocol.recoveryMode:
+            while protocol.recoveryMode:
+                self.liquidate(protocol, 0)
+        else:
+            for i in range(len(protocol.balanceSheet)):
+                if (
+                    protocol.balanceSheet[i, 2] / protocol.balanceSheet[i, 1]
+                ) < protocol.calculate_liquidation_threshold(
+                    self, protocol.balanceSheet[i, 2]
+                ):
+                    self.liquidate(protocol, i)
 
-    def liquidate(self, dynamicLBProtocol, i): 
-        dynamicLBProtocol.totalD -= dynamicLBProtocol.balanceSheet[i][1]
-        dynamicLBProtocol.totalC -= dynamicLBProtocol.balanceSheet[i][2]
-        liquidatorReward = dynamicLBProtocol.balanceSheet[i][1] * 0.005 + utils.estimate_execution_cost(dynamicLBProtocol.vol, dynamicLBProtocol.balanceSheet[i][1], dynamicLBProtocol.price, True)
-        dynamicLBProtocol.liqidationRewards += dynamicLBProtocol.balanceSheet[i][2] - dynamicLBProtocol.balanceSheet[i][1] - liquidatorReward
+    def liquidate(self, protocol: DynamicLBProtocol, i: int):
+        protocol.totalD -= protocol.balanceSheet[i][1]
+        protocol.totalC -= protocol.balanceSheet[i][2]
+        liquidatorReward = protocol.balanceSheet[i][
+            1
+        ] * 0.005 + utils.estimate_execution_cost(
+            protocol.vol,
+            protocol.balanceSheet[i][1],
+            protocol.price,
+            True,
+        )
+        protocol.liqidationRewards += (
+            protocol.balanceSheet[i][2] - protocol.balanceSheet[i][1] - liquidatorReward
+        )
         self.reward += liquidatorReward
 
-        dynamicLBProtocol.balanceSheet.delete(i) 
-        dynamicLBProtocol.set_recovery_mode(dynamicLBProtocol.vol)
-        
+        protocol.balanceSheet.delete(i)
+        protocol.set_recovery_mode(protocol.vol)
+
+        return
