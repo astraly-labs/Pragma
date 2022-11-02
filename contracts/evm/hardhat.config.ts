369b1300f535cc6714e831e4f54f787d9a74a14c
Eugene Huang <eugenehuang@42s-MacBook-Pro.local>
2022-11-02 11:17:16 -0400

    added polygon and optimism

--- contracts/evm/hardhat.config.ts
+++ contracts/evm/hardhat.config.ts
@@ -1,33 +1,42 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "@matterlabs/hardhat-zksync-deploy";
-import "@matterlabs/hardhat-zksync-solc";
+// import "@matterlabs/hardhat-zksync-deploy";
+// import "@matterlabs/hardhat-zksync-solc";
 
 const config: HardhatUserConfig = {
-  zksolc: {
-    version: "1.2.0",
-    compilerSource: "docker",
-    settings: {
-      optimizer: {
-        enabled: true,
-      },
-      experimental: {
-        dockerImage: "matterlabs/zksolc",
-        tag: "v1.2.0"
-      }
-    },
-  },
-  zkSyncDeploy: {
-    zkSyncNetwork: "https://zksync2-testnet.zksync.dev",
-    ethNetwork: "goerli", // Can also be the RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
-  },
+  // zksolc: {
+  //   version: "1.2.0",
+  //   compilerSource: "docker",
+  //   settings: {
+  //     optimizer: {
+  //       enabled: true,
+  //     },
+  //     experimental: {
+  //       dockerImage: "matterlabs/zksolc",
+  //       tag: "v1.2.0"
+  //     }
+  //   },
+  // },
+  // zkSyncDeploy: {
+  //   zkSyncNetwork: "https://zksync2-testnet.zksync.dev",
+  //   ethNetwork: "goerli", // Can also be the RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
+  // },
   networks: {
     goerli: {
       url: "https://rpc.ankr.com/eth_goerli",
       chainId: 5,
-      zksync: false,
+      // zksync: false,
       accounts: [process.env.PRIVATE_KEY!],
     },
+    polygonTestnet: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      chainId: 80001,
+      accounts: [process.env.PRIVATE_KEY!],
+    },
+    optimismTestnet: {
+      url: "https://goerli.optimism.io",
+      chainId: 420
+    }
   },
   solidity: {
     version: "0.8.17",
