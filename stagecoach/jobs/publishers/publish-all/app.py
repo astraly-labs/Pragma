3aee5e47399d7bf3d2b7285681c5cc917766360e
Eugene Huang <eugenehuang@42s-MacBook-Pro.local>
2022-11-10 14:23:51 -0500

    removed FtxFetcher

--- stagecoach/jobs/publishers/publish-all/app.py
+++ stagecoach/jobs/publishers/publish-all/app.py
@@ -11,7 +11,6 @@ from empiric.publisher.fetchers import (
     BitstampFetcher,
     CexFetcher,
     CoinbaseFetcher,
-    FtxFetcher,
     TheGraphFetcher,
 )
 
@@ -61,7 +60,6 @@ async def _handler(assets):
                 BitstampFetcher,
                 CexFetcher,
                 CoinbaseFetcher,
-                FtxFetcher,
                 TheGraphFetcher,
             )
         ]
