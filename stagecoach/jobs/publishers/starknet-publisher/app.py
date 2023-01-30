ccfde3d08430d0fcb81c89a08f00cb3ef6471998
Evolve <Artevolve@yahoo.com>
2023-01-30 15:17:01 +0100

    fix: publisher imports ✍️

--- stagecoach/jobs/publishers/starknet-publisher/app.py
+++ stagecoach/jobs/publishers/starknet-publisher/app.py
@@ -7,7 +7,12 @@ from empiric.core import SpotEntry
 from empiric.core.logger import get_stream_logger
 from empiric.publisher.assets import get_spot_asset_spec_for_pair_id
 from empiric.publisher.client import EmpiricPublisherClient
-from empiric.publisher.fetchers import BitstampFetcher, CexFetcher, CoinbaseFetcher
+from empiric.publisher.fetchers import (
+    BitstampFetcher,
+    CexFetcher,
+    CoinbaseFetcher,
+    AscendexFetcher,
+)
 
 logger = get_stream_logger()
 
