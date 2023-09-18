69756e261fc642869c37b05318e92ccd5c80cc26
Evolve <artevolve@yahoo.com>
2023-09-18 17:47:45 +0200

    :art: toggle outline

--- pragma-ui/src/pages/index.tsx
+++ pragma-ui/src/pages/index.tsx
@@ -118,7 +118,7 @@ const IndexPage = () => {
             className="peer sr-only"
             onChange={toggleNetwork}
           />
-          <div className="peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:top-[2px] after:left-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-green-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-green-300 dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-green-800"></div>
+          <div className="peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:top-[2px] after:left-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-green-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-green-800"></div>
           <span className="ml-3 text-sm font-medium text-white dark:text-gray-300">
             Switch to Mainnet
           </span>
