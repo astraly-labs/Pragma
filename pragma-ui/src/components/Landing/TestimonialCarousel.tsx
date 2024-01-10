243b90adecce6974a08d9a6334a02780c9bd45ef
Mattegoat <matteo.georges74@hotmail.fr>
2024-01-10 12:03:26 +0100

    fix: build errors

--- pragma-ui/src/components/Landing/TestimonialCarousel.tsx
+++ pragma-ui/src/components/Landing/TestimonialCarousel.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import GreenText from "../common/GreenText";
 import GreenUpperText from "../common/GreenUpperText";
 import styles from "./styles.module.scss";
