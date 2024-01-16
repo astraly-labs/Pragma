93c21b1861563020844dbc53ebe622eb7db56b8d
Mattegoat <matteo.georges74@hotmail.fr>
2024-01-16 13:48:57 +0100

    fix: backdrop filter

--- pragma-ui/src/components/common/BoxContainer.tsx
+++ pragma-ui/src/components/common/BoxContainer.tsx
@@ -19,7 +19,7 @@ const BoxContainer: React.FC<ContainerProps> = ({
 }) => (
   <div
     className={classNames(
-      "flex w-full flex-col items-center gap-8 p-3 pb-0 sm:p-8 sm:pb-0 lg:flex-row",
+      "relative z-10 flex w-full flex-col items-center gap-8 p-3 pb-0 sm:p-8 sm:pb-0 lg:flex-row",
       modeOne ? "" : "",
       className
     )}
