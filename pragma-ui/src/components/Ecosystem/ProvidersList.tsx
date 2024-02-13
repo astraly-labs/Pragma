1d96aa6f09caf45a0e3727bf3b11e67af860529a
0xevolve <Artevolve@yahoo.com>
2024-02-13 17:25:47 +0100

    prettier

--- pragma-ui/src/components/Ecosystem/ProvidersList.tsx
+++ pragma-ui/src/components/Ecosystem/ProvidersList.tsx
@@ -2,8 +2,23 @@ import React from "react";
 import styles from "./styles.module.scss";
 import classNames from "classnames";
 
-const providersData = [ 
-  { logo: "/assets/ecosystem/skynet.svg", description: "Skynet Trading is thrilled to be at the forefront of enhancing trust and accuracy in the crypto market, contributing our valuable data through Pragma's advanced zk-enabled technology, a pivotal step towards a more transparent and reliable digital asset ecosystem.", }, { logo: "/assets/ecosystem/flowdesk.svg", description: "We are happy to partner with Pragma as a data provider. Their verifiable computational feeds are a key component to support the next generation of Defi protocols and we are excited to take part in this project", }, { logo: "/assets/ecosystem/avnu.svg", description: "We're thrilled to provide our top-tier data from our solvers to help secure DeFi on Starknet", }, ];
+const providersData = [
+  {
+    logo: "/assets/ecosystem/skynet.svg",
+    description:
+      "Skynet Trading is thrilled to be at the forefront of enhancing trust and accuracy in the crypto market, contributing our valuable data through Pragma's advanced zk-enabled technology, a pivotal step towards a more transparent and reliable digital asset ecosystem.",
+  },
+  {
+    logo: "/assets/ecosystem/flowdesk.svg",
+    description:
+      "We are happy to partner with Pragma as a data provider. Their verifiable computational feeds are a key component to support the next generation of Defi protocols and we are excited to take part in this project",
+  },
+  {
+    logo: "/assets/ecosystem/avnu.svg",
+    description:
+      "We're thrilled to provide our top-tier data from our solvers to help secure DeFi on Starknet",
+  },
+];
 
 const ProvidersList = () => (
   <div
