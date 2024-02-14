929fc2b2a632e0c63c5298a0bf639e06f6f253a6
Mattegoat <matteo.georges74@hotmail.fr>
2024-02-14 13:40:48 +0100

    fix: build

--- pragma-ui/src/components/Landing/CodeSnippet.tsx
+++ pragma-ui/src/components/Landing/CodeSnippet.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import styles from "./styles.module.scss";
 import GreenBox from "../common/GreenBox";
 import { useState } from "react";
 import { Tab } from "@headlessui/react";
