1f09ed108331d5971acdc3f0129c3bffaa8bf739
Mattegoat <matteo.georges74@hotmail.fr>
2024-02-14 10:14:12 +0100

    fix: analytics

--- pragma-ui/src/pages/_document.tsx
+++ pragma-ui/src/pages/_document.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { Html, Head, Main, NextScript } from "next/document";
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/react";
 
 /**
  * Used to set the lang property according to:
@@ -15,8 +13,6 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <Analytics />
-        <SpeedInsights />
       </body>
     </Html>
   );
