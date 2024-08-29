2137df0c626d95ae2d7f9aa98553a49c60b9a0f3
JordyRo1 <jordy.romu@orange.fr>
2024-08-29 15:37:09 +0200

    fix(addresses): oo and oracle ancillary contract addresses

--- pragma-ui/src/pages/constants.tsx
+++ pragma-ui/src/pages/constants.tsx
@@ -1,7 +1,7 @@
 export const OO_CONTRACT_ADDRESS = {
-  sepolia: '0x04d559e9a6bedc3cea5cf87961e24a87340699ff793ad1c6e3349fb8d6a8e91f', 
+  sepolia: '0x044ac84b04789b0a2afcdd2eb914f0f9b767a77a95a019ebaadc28d6cacbaeeb', 
   mainnet: ''};
-export const ORACLE_ANCILLARY_ADDRESS = {sepolia: '0x0161cd3731ef0ecfb2b4c51f0ff5b195adb027dccee811ac6432331315491225', mainnet: ''};
+export const ORACLE_ANCILLARY_ADDRESS = {sepolia: '0x00b7940968024a3f9963968226e07904343ba95f2773dadc2fb98863430f1187', mainnet: ''};
 export const CURRENCIES = {
     sepolia: [{name: "ETH", address: '0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7'}],
     mainnet: []
