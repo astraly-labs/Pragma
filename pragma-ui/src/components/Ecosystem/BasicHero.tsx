3d51ed5e87c363630d5c8eb5695cb4ee8c14e407
Mattegoat <matteo.georges74@hotmail.fr>
2024-09-30 17:56:46 +0200

    fix: build

--- pragma-ui/src/components/Ecosystem/BasicHero.tsx
+++ pragma-ui/src/components/Ecosystem/BasicHero.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import styles from "./styles.module.scss";
 import classNames from "classnames";
 import GreenText from "../common/GreenText";
 import { ButtonLink } from "../common/Button";
