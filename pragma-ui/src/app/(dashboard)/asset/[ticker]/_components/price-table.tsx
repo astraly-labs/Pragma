ae004f3a1a2dbcc14cda23038af8c9df166607f9
Ismail Ghallou <ismai23l@hotmail.com>
2025-04-15 12:15:50 +0100

    chore: remove checkpoints for api source and tx hash (#462)

--- pragma-ui/src/app/(dashboard)/asset/[ticker]/_components/price-table.tsx
+++ pragma-ui/src/app/(dashboard)/asset/[ticker]/_components/price-table.tsx
@@ -7,9 +7,11 @@ import { truncateTxHash } from "@/utils";
 export const PriceTable = ({
   components,
   decimals,
+  network,
 }: {
   components: Price[];
   decimals: number;
+  network: string;
 }) => {
   return (
     <div className="z-1 w-full flex-col justify-between gap-0 green-box">
@@ -26,16 +28,23 @@ export const PriceTable = ({
           <div className="flex flex-row gap-2 font-mono text-xs text-LightGreenFooter md:tracking-wider">
             Price
           </div>
-          <div className="flex flex-row gap-2 font-mono text-xs text-LightGreenFooter md:tracking-wider">
-            Hash
-          </div>
+          {network === "mainnet" && (
+            <div className="flex flex-row gap-2 font-mono text-xs text-LightGreenFooter md:tracking-wider">
+              Hash
+            </div>
+          )}
           <div className="flex flex-row gap-2 font-mono text-xs text-LightGreenFooter md:tracking-wider">
             Last Updated
           </div>
           <div className="flex flex-row gap-2 font-mono text-xs text-LightGreenFooter md:tracking-wider"></div>
         </div>
         {components.map((component, index) => (
-          <PriceItem key={index} component={component} decimals={decimals} />
+          <PriceItem
+            key={index}
+            component={component}
+            decimals={decimals}
+            network={network}
+          />
         ))}
       </div>
     </div>
@@ -45,9 +54,11 @@ export const PriceTable = ({
 const PriceItem = ({
   component,
   decimals,
+  network,
 }: {
   component: Price;
   decimals: number;
+  network: string;
 }) => {
   return (
     <div className={styles.priceComp}>
@@ -70,18 +81,20 @@ const PriceItem = ({
       <div className="flex flex-row gap-2 font-mono text-sm text-lightGreen md:tracking-wider">
         {parseInt(component.price, 16) / 10 ** decimals}
       </div>
-      <Link
-        className=" flex cursor-pointer flex-row gap-2 font-mono text-sm text-lightGreen md:tracking-wider"
-        href={`https://voyager.online/tx/${component.tx_hash}`}
-      >
-        {truncateTxHash(component.tx_hash)}
-        <Image
-          height={16}
-          width={16}
-          alt="Link"
-          src="/assets/vectors/outLink.svg"
-        />
-      </Link>
+      {network === "mainnet" && (
+        <Link
+          className=" flex cursor-pointer flex-row gap-2 font-mono text-sm text-lightGreen md:tracking-wider"
+          href={`https://voyager.online/tx/${component.tx_hash}`}
+        >
+          {truncateTxHash(component.tx_hash)}
+          <Image
+            height={16}
+            width={16}
+            alt="Link"
+            src="/assets/vectors/outLink.svg"
+          />
+        </Link>
+      )}
 
       <div className="flex flex-row gap-2 overflow-auto font-mono text-sm text-lightGreen md:tracking-wider">
         {new Date(component.timestamp * 1000).toLocaleString()}
