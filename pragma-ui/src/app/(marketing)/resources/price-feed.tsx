0425169f4e8062ce6a265c5b721eb9dccaad3656
smakosh <ismai23l@hotmail.com>
2025-05-02 21:57:51 +0100

    fix: fetch asset fixed

--- pragma-ui/src/app/(marketing)/resources/price-feed.tsx
+++ pragma-ui/src/app/(marketing)/resources/price-feed.tsx
@@ -14,8 +14,8 @@ import { initialAssets } from "@/lib/endpoints";
 type Assets = {
   ticker: string;
   lastPrice: any;
-  variation24h: number;
-  relativeVariation24h: number;
+  variation24h: number | null;
+  relativeVariation24h: number | null;
   priceData: {
     time: UTCTimestamp;
     value: number;
